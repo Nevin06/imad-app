945556ed088eb70e363b931f59c440a025e7c234
Nevin06 <thomasnevink@gmail.com>
2017-09-08 00:40:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,13 @@ var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft = '100px';
 };
+
+
+var marginLeft=0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight,100)
+};
