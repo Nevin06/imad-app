fc1763aca4b033a9632ca38e5501aed9c75d8da8
Nevin06 <thomasnevink@gmail.com>
2017-09-08 19:55:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 console.log('Loaded!');
 
 //change the text of the main-text div
@@ -11,7 +12,7 @@ img.onclick = function () {
     img.style.marginLeft = '100px';
 };
 
-
+*/
 var marginLeft=0;
 function moveRight () {
     marginLeft = marginLeft + 1;
@@ -19,4 +20,22 @@ function moveRight () {
 }
 img.onclick = function () {
     var interval = setInterval(moveRight,50)
+};
+
+
+//Counter code
+var button = document.getElementById('counter');
+var counter=0;
+
+button.onclick = function () {
+    
+    //Make a request to the counter endpoint.
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter=counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+
 };
