1fd61c41bb18698a906a9174415da4be04fc7a04
Nevin06 <thomasnevink@gmail.com>
2017-09-08 23:33:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/counter', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {     //  submit-name?name=xxxx
+app.get('/submit-name', function (req, res) {     //  submit-name?name=xxxx
     //Get the name from the request
     //var name = req.params.name;
     var name = req.query.name;
