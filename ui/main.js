64eb17f8ceb4bb7de1287e5ab7e270ab3efb3f44
Nevin06 <thomasnevink@gmail.com>
2017-09-08 23:55:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -67,12 +67,28 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //Make a request to the server and send the name 
     
-    //Capture a list of names and render it as a list.
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list+= '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+        //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState == XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status == 200){
+                //Capture a list of names and render it as a list.
+                var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                            list+= '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        //Not done yet
     };
+    
+        //Make the request
+    request.open('GET','http://thomasnevink.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
